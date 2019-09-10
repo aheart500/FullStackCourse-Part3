5543c92901ac7da6106ffd532307b7d7e255d8db
Mohamed Nasser <aheart500@yahoo.com>
2019-09-10 05:42:47 +0200

    Production build

---
+++ build/precache-manifest.d7566bfb2e47af326256c6b1f7fbc205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8db6f94252f955074682abba30c6bea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1575f0ba78389a5a8fb1",
+    "url": "/static/css/main.17e5fe4b.chunk.css"
+  },
+  {
+    "revision": "ad9007414a2f42163073",
+    "url": "/static/js/2.7ec20218.chunk.js"
+  },
+  {
+    "revision": "1575f0ba78389a5a8fb1",
+    "url": "/static/js/main.adc7453a.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
