269d339ca00175c847dccc79444678c8915dcfcd
Mohamed Nasser <aheart500@yahoo.com>
2019-09-11 21:02:25 +0200

    'eslint'

--- mongo.js
+++ mongo.js
@@ -1,29 +1,16 @@
 const mongoose = require('mongoose')
-
 if(process.argv.length< 3){
     console.log('Please Enter your password')
     process.exit(1)
 }
-
 const password = process.argv[2]
 const url =`mongodb+srv://Nasser:${password}@cluster0-gwux8.mongodb.net/phonebook?retryWrites=true&w=majority`
-
-
 mongoose.connect(url,{useNewUrlParser : true,useUnifiedTopology:true})
-
-
-
 const personSchema = new mongoose.Schema({
     name : String,
     number : String,
 })
-
 const Person = mongoose.model('Person',personSchema)
-
-
-
-
-
 if(process.argv.length === 3){
     Person.find({}).then((result)=>{
         console.log("Phonebook : ")
